093ef6ad003b5e0c86e4d92c39d8b3b86190c177
MrDustinMiller <dustin.miller@mscoding.org>
2023-02-18 21:57:27 -0600

    changed exiting modal from x to window

--- scripts/form.js
+++ scripts/form.js
@@ -2,7 +2,6 @@ const form = document.querySelector('.new-book-form');
 const newBookButton = document.querySelector('.fa-plus');
 const submitBtn = document.querySelector('.submit');
 const overlay = document.querySelector('.overlay');
-const xMark = document.querySelector('.fa-xmark');
 
 newBookButton.addEventListener('click', () => {
   form.style.visibility = 'visible';
@@ -14,7 +13,9 @@ submitBtn.addEventListener('click', () => {
   overlay.style.visibility = 'hidden';
 });
 
-xMark.addEventListener('click', () => {
-  form.style.visibility = 'hidden';
-  overlay.style.visibility = 'hidden';
+overlay.addEventListener('click', () => {
+  if (form.style.visibility === 'visible') {
+    form.style.visibility = 'hidden';
+    overlay.style.visibility = 'hidden';
+  }
 });
